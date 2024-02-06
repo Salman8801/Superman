c3227f2a319907b643561de4fe6fa6a1e19e8e70
Salman8801 <134062350+Salman8801@users.noreply.github.com>
2024-02-06 15:07:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,9 +50,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filesg')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Ziplinker.net')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'dabfbef2ab8de20ab3bb7860e8853bee59131a53')
+SHORTLINK_API = environ.get('SHORTLINK_API', '25371d209e7dd116bbf42a90b8e9c70b558be7c1')
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'Ziplinker.net')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'dabfbef2ab8de20ab3bb7860e8853bee59131a53')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '25371d209e7dd116bbf42a90b8e9c70b558be7c1')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
